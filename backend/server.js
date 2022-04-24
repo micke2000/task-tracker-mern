90cf5044fa02f8f3d9446e73890f6a9709295db5
micke2000 <michal.mickiewicz@pollub.edu.pl>
2022-04-24 16:27:37 +0200

    deploy 13

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const path = require("path");
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 const colors = require("colors");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
